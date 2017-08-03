4bd5324c6bd0350876ab1741b96d7a3048d2ed1c
pyramidk <948206388@qq.com>
2017-08-03 22:00:24 +0800

    :tada: Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div>
+      home page
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
